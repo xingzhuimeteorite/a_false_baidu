ade9ad46ca848e6067e8b49935c281c7cf747357
yang_24318_sys <yang_sys@hand.com>
2019-10-10 10:14:42 +0800

    增加注释，完善输入提示

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 
-
+# 父类
 class SearchEnginBase(object):
 	def __init__(self):
 		pass
-	
+	#搜索集 读取文本
 	def add_corpus(self,file_path):
 		with open(file_path,'r') as fin:
 			text = fin.read()
@@ -20,9 +20,10 @@ class SimpleEngine(SearchEnginBase):
 	def __init__(self):
 		super(SimpleEngine,self).__init__()
 		self.__id_to_texts = {}
+	#键值对，定义文件名 文件
 	def process_corpus(self,id,text):
 		self.__id_to_texts[id] = text
-		
+	#搜索 返回结果集	
 	def search(self,query):
 		results = []
 		for id,text in self.__id_to_texts.items():
@@ -33,9 +34,9 @@ class SimpleEngine(SearchEnginBase):
 def main(search_engine):
 	for file_path in ['1.txt','2.txt','3.txt','4.txt','5.txt']:
 		search_engine.add_corpus(file_path)
-		
+	#搜索引擎	
 	while True:
-		query = input()
+		query = input('请输入查询内容：')
 		results = search_engine.search(query)
 		print('found{}result(s):'.format(len(results)))
 		for result in results:
